e0bd1ef23d18369bf86a0097e762f645ea50219b
Gilberts Ahumada <gilbertsahumada@Gilbertss-MacBook-Pro.local>
2024-06-13 16:51:16 -0400

    first commit

---
+++ app/providers.tsx
@@ -0,0 +1,40 @@
+"use client"
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+    getDefaultConfig,
+    RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import { WagmiProvider } from 'wagmi';
+import {
+    mainnet,
+    polygon,
+    optimism,
+    arbitrum,
+    base,
+    avalancheFuji
+} from 'wagmi/chains';
+import {
+    QueryClientProvider,
+    QueryClient,
+} from "@tanstack/react-query";
+
+const config = getDefaultConfig({
+    appName: 'Obsevadores!',
+    projectId: 'YOUR_PROJECT_ID',
+    chains: [mainnet, polygon, optimism, arbitrum, base, avalancheFuji],
+    ssr: true, // If your dApp uses server side rendering (SSR)
+});
+
+const queryClient = new QueryClient();
+
+export function Providers({ children }: { children: React.ReactNode }) {
+    return (
+        <WagmiProvider config={config}>
+            <QueryClientProvider client={queryClient}>
+                <RainbowKitProvider>
+                    {children}
+                </RainbowKitProvider>
+            </QueryClientProvider>
+        </WagmiProvider>
+    )
+}
