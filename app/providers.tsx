0955efd51487a132d6e3d023a24ab5cd23c1fdde
Gilberts Ahumada <gilbertsahumada@Gilbertss-MacBook-Pro.local>
2024-07-02 15:22:21 -0400

    updating config

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,10 +1,10 @@
 "use client"
 import '@rainbow-me/rainbowkit/styles.css';
-import {
-    getDefaultConfig,
+import {    
     RainbowKitProvider,
+    connectorsForWallets
 } from '@rainbow-me/rainbowkit';
-import { WagmiProvider } from 'wagmi';
+import { WagmiProvider, createConfig, http } from 'wagmi';
 import {
     mainnet,
     polygon,
@@ -17,21 +17,53 @@ import {
     QueryClientProvider,
     QueryClient,
 } from "@tanstack/react-query";
+import { intmaxwalletsdk } from "intmax-walletsdk/rainbowkit";
 
+const additionalWallets = [
+    intmaxwalletsdk({
+        wallet: {
+            url: "https://intmaxwallet-sdk-wallet.vercel.app/",
+            name: "IntmaxWallet ETH Volcano",
+            iconUrl: "https://intmaxwallet-sdk-wallet.vercel.app/vite.svg",
+        },
+        metadata: {
+            name: "Rainbow-Kit Demo",
+            description: "Rainbow-Kit Demo",
+            icons: ["https://intmaxwallet-sdk-wallet.vercel.app/vite.svg"],
+        },
+    }),
+];
 
+const connectors =  connectorsForWallets(
+    [
+      {
+        groupName: "Best Walletz",
+        wallets: additionalWallets,
+      },
+    ],
+    { projectId: "N/A", appName: "ETH Volcano Example" },
+  )
 
 export function Providers({ children }: { children: React.ReactNode }) {
-    const config = getDefaultConfig({
-        appName: 'Obsevadores!',
-        projectId: 'YOUR_PROJECT_ID',
+
+    const wagmiConfig = createConfig({                 
         chains: [mainnet, polygon, optimism, arbitrum, base, avalancheFuji],
-        ssr: true, // If your dApp uses server side rendering (SSR)
+        transports: {
+            [polygon.id]: http("https://polygon-rpc.com"),
+            [mainnet.id]: http("https://mainnet-rpc.com"),
+            [optimism.id]: http("https://optimism-rpc.com"),
+            [arbitrum.id]: http("https://arbitrum-rpc.com"),
+            [base.id]: http("https://base-rpc.com"),
+            [avalancheFuji.id]: http("https://avalanche-fuji-rpc.com"),
+        },
+        connectors,
     });
     
+
     const queryClient = new QueryClient();
-    
+
     return (
-        <WagmiProvider config={config}>
+        <WagmiProvider config={wagmiConfig}>
             <QueryClientProvider client={queryClient}>
                 <RainbowKitProvider>
                     {children}
